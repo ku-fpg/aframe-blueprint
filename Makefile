3c764ffbdaed17a1982afa4d6b7ad021f4adf1a8
Andy Gill <andygill@ku.edu>
2016-09-21 14:35:32 -0500

    setting up 2nd demo

--- Makefile
+++ Makefile
@@ -2,10 +2,17 @@
 #
 # Examples
 #
+boot::  examples/hello-world/index.html \
+        examples/hello-edit/index.html
+	echo done        
+
 examples/hello-world/index.html: examples/src/Main.hs
 	mkdir -p examples/hello-world
 	cabal exec runghc examples/src/Main.hs hello-world $@
 
+examples/hello-edit/index.html: examples/src/Main.hs
+	mkdir -p examples/hello-edit
+	cabal exec runghc examples/src/Main.hs hello-edit $@
 
 server::
 	npm run dev &
--- examples/src/Main.hs
+++ examples/src/Main.hs
@@ -43,6 +43,41 @@ example = scene $ do
     position (0,0,5.8)
     camera $ return ()
 
+
+editExample :: AFrame
+editExample = scene $ do
+
+  sphere $ do
+    position (0,1.25,-1)
+    radius   1.25
+    color    "#EF2D5E"
+
+  box $ do
+    position (-1,0.5,1)
+    rotation (0,45,0)
+    width    1
+    height   1
+    scale   (1,1,1)
+    color   "#4CC3D9"
+
+  cylinder $ do
+    position (1,0.75,1) 
+    radius   0.5
+    height   1.5
+    color    "#FFC65D"
+  
+  plane $ do
+    rotation (-90,0,0)
+    width  4
+    height 4
+    color "#7BC8A4"
+
+  sky $ color "#ECECEC"
+  
+  entity $ do
+    position (0,0,3.8)
+    camera $ return ()
+
 main :: IO ()
 main = do
         args <- getArgs
@@ -51,4 +86,5 @@ main = do
 
 main2 :: [String] -> IO ()
 main2 ("hello-world":args) = webPage args example
+main2 ("hello-edit":args) = webPage args editExample
 main2 _ = error "usage: aframe-blueprint-example example-name output.html"
