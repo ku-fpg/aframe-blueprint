bbc3b5aba9788e3941c99c64ac360790e1dc2547
Andy Gill <andygill@ku.edu>
2017-01-17 13:49:29 -0600

    Debugging intersectQuads

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs, ScopedTypeVariables, InstanceSigs, OverloadedStrings #-}
 module Main where
 	
 import Control.Applicative ((<|>))
@@ -6,9 +7,17 @@ import Control.Monad
 import Data.Monoid ((<>))
 import Debug.Trace
 
+import System.Random
+
 import Text.AFrame.Geometry
 
 import Test.QuickCheck
+
+import System.IO.Unsafe
+
+import Text.AFrame.WebPage 
+import qualified Text.AFrame.DSL as DSL
+
 
 ------------------------------------------------------------------------------------
 -- Testing
@@ -38,30 +47,66 @@ plane w h = Quad (Position (-w/2,h/2,0)) (Position (-w/2,-h/2,0)) (Position (w/2
 
 -- For testing
 
-newtype Degree = Degree (Double,Double,Double)
- deriving Show
-
-instance Arbitrary Degree where
-  arbitrary = (\ a b c -> Degree (a,b,c))
+instance (Random a, Floating a, Arbitrary a) =>  Arbitrary (Rotation a) where
+  arbitrary = (\ a b c -> Rotation (a,b,c))
                      <$> choose (-360,360)
                      <*> choose (-360,360)
                      <*> choose (-360,360)
 
 -- Two never-indeterminate quads
-data Pair = Pair (Quad Double) (Quad Double)
+data Pair a = Pair (Quad a) (Quad a)
+ deriving Show
+
+instance Geometric Pair where
+  run :: Floating a => Geometry a -> Pair a -> Pair a
+  run g (Pair q1 q2) = Pair (run g q1) (run g q2)
+
+instance (Random a, Floating a, Arbitrary a) => Arbitrary (Pair a) where
+  arbitrary = (\ (Rotation r1) (Rotation r2) -> Pair (run (position (-1.5,0,0) <> rotation YXZ r1) $ plane 2 2)
+			       (run (position ( 1.5,0,0) <> rotation YXZ r2) $ plane 2 2))
+                     <$> arbitrary
+                     <*> arbitrary
 
 -- Check comparePolygon function
-prop_compare (Degree lr) (Degree rr) (Degree cr) =
+prop_compare (pair :: Pair Double) (Rotation cr :: Rotation Double) =
 --  trace (splat $ fmap (fmap draw) $ intersectQuads [p1,p2]) False
     forAll (elements points) $ \ (x,y) ->
     case (intersectQuad p1 (x,y), intersectQuad p2 (x,y)) of
       (Just z1,Just z2) -> case cmp of
         Behind          -> True ==> if z1 < z2 then True else trace (msg (x,y) z1 z2) False
         InFront         -> True ==> if z1 > z2 then True else trace (msg (x,y) z1 z2) False
-        Disjoint        -> True ==> True
-        Indeterminate   -> True ==> True -- trace (msg (x,y) z1 z2) False -- error "should never be indeterminate"
+        Disjoint        -> False ==> True
+        Indeterminate   -> True ==> unsafePerformIO $ failed (x,y) z1 z2 
+				 -- trace (msg (x,y) z1 z2) False -- error "should never be indeterminate"
       _                 -> False ==> True
    where
+     failed :: (Double,Double) -> Double -> Double -> IO Bool
+     failed (x,y) z1 z2 = do
+	     webPage ["fail.html"]  $ DSL.scene $ do
+		     DSL.cylinder $ do
+			 DSL.color "green"
+			 DSL.height 10
+			 DSL.radius 0.05
+			 DSL.rotation (-90,0,0)
+			 DSL.position (f x,f y,-5)
+		     face p1 "red"
+		     face p2 "blue"
+		     return ()
+	     return False
+
+     face (Quad a1 a2 a3 a4) col = do
+	     sequence_ 
+	        [ DSL.sphere $ do
+		    DSL.color col
+		    DSL.radius 0.1
+		    DSL.position (f x,f y,f z)
+		| Position (x,y,z) <- [a1,a2,a3,a4]
+		]
+
+     f = fromRational . toRational
+     
+     Pair p1 p2 = run common pair
+
      -- ((x,y),z1,z2,cmp)
      msg (x,y) z1 z2 = "\n" ++ 
        unlines [ "x: " ++ show x
@@ -83,13 +128,13 @@ prop_compare (Degree lr) (Degree rr) (Degree cr) =
 
      common :: Geometry Double
      common = position (0,0,-5) <> rotation YXZ cr
-
+{-
      p1 :: Quad Double
      p1 = run (common <> position (-1.5,0,0) <> rotation YXZ lr) $ plane 2 2
 
      p2 :: Quad Double
      p2 = run (common <> position ( 1.5,0,0) <> rotation YXZ rr) $ plane 2 2
-      
+-}      
      cmp :: Stacking
      cmp = comparePolygon p1 p2
 
